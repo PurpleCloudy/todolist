7ad728f9f06c144571dd7b62dc56f36d506c5d62
PurpleCloudy <forstudy546@gmail.com>
2023-01-25 21:36:59 +0300

    fourth commit

--- main/models.py
+++ main/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class ChoosingTask(models.Model):
+    MONTH_CHOICE = (
+        ('JA', 'January'),
+        ('FE', 'February'),
+        ('MR', 'March'),
+        ('AP', 'April'),
+        ('MY', 'May'),
+        ('JN', 'June'),
+        ('JL', 'July'),
+        ('AU', 'August'),
+        ('SP', 'September'),
+        ('OC', 'October'),
+        ('NV', 'November'),
+        ('DC', 'December'),
+    )
+
+    DIFF = (
+        ('ST','simple task'),
+        ('CT','complex task'),
+    )
+
+    month = models.CharField(max_length=9, choices=MONTH_CHOICE, default="JANUARY")
+    
+    day = models.PositiveSmallIntegerField()
+
+    difficulty = models.CharField(max_length=2, choices=DIFF, default='ST')    
+
